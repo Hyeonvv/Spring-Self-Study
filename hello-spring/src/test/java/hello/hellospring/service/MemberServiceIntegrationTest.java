3ba25e1ad478dea215174218d1520128276ef53c
Hyeonvv <pphhww5017@gmail.com>
2023-05-26 21:15:16 +0900

    :pencil2:: [박현우] 스프링 통합 테스트

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -1,33 +1,28 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
+import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
-// 이렇게 자바 코드로만 최소한의 단위로 테스트 하는 것을 단위 테스트라고 한다.
-// 스프링 컨테이너 없이 하는 테스트가 더 좋은 테스트
-// Class 에서 cmd + shift + t -> 테스트 틀 자동 생성
-class MemberServiceTest {
+// 스프링 컨테이너와 DB까지 연동해서 테스트 하는것을 통합 테스트라고 한다.
+@SpringBootTest // 스프링 컨테이너와 테스트를 함께 실행한다.
+@Transactional // Transactional 을 테스트에 사용 시 테스트 하나마다(메소드마다) 시작 전에 Transaction 을 실행하고, 테스트 실행 후 Rollback 한다. -> 다음 테스트에 영향 X
+class MemberServiceIntegrationTest {
 
+    // Test 할 때는 보통 필드 주입(Field Injection)을 통해 간편하게 사용한다.
+    @Autowired
     MemberService memberService;
-    MemoryMemberRepository memberRepository;
-
-    @BeforeEach
-    public void beforeEach() {
-        memberRepository = new MemoryMemberRepository();
-        memberService = new MemberService(memberRepository);
-    }
-
-    @AfterEach
-    public void afterEach() {
-        memberRepository.clearStore();
-    }
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     void 회원가입() {
@@ -57,16 +52,7 @@ class MemberServiceTest {
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
 
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-/*
-        try {
-            memberService.join(member2);
-            fail();
-        } catch (IllegalStateException e) {
-            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
-        }
-*/
 
-        //then
     }
 
     @Test
