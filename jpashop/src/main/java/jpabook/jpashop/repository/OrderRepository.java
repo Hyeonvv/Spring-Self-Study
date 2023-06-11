79d945a4e9c2611e75244c9cd60b5dc6dd76cafb
Hyeonvv <pphhww5017@gmail.com>
2023-06-11 18:43:30 +0900

    study: 주문 Repository 구현

---
+++ jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -0,0 +1,26 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Order;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepository {
+
+    private final EntityManager em;
+
+    public void save(Order order) {
+        em.persist(order);
+    }
+
+    public Order findOne(Long orderId) {
+        return em.find(Order.class, orderId);
+    }
+
+    // 검색을 통한 리스트 조회(나중에 이어서 작성)
+//    public List<Order> findAll(OrderSearch orderSearch) {}
+}
