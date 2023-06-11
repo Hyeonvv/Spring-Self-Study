22138775ab397edf1525577d58d77e4018ab9b45
Hyeonvv <pphhww5017@gmail.com>
2023-06-11 15:17:17 +0900

    study: 상품 Entity에 비즈니스 로직 추가(수량 변경)

--- jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,7 +12,7 @@ import java.util.List;
 @Entity
 @Inheritance(strategy = InheritanceType.SINGLE_TABLE) // SINGLE_TABLE 전략 -> 한 테이블에 다 몰아넣겠다.
 @DiscriminatorColumn(name = "dtype") // 구분 지을때 사용? 한다.
-@Getter @Setter
+@Getter @Setter // Setter 로 값을 변경하는 것이 아닌 필요 메소드에 의해서 값을 변경하는 것이 좋다.
 public abstract class Item {
 
     @Id
@@ -25,4 +26,27 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+    // 엔티티 자체가 해결할 수 있는 것들은 엔티티 안에 비즈니스 로직을 넣는 것이 좋다. 객체 지향적이고, 응집도가 높음 -> 도메인 주도 설계
+    // 현재 stockQuantity 는 Item 엔티티 내에 존재하기 때문에 Item 엔티티 내에 비즈니스 로직을 설계
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
+
+
 }
