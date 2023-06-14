cab45f73bae8cd0a7855542d9280788a0899c6b1
Hyeonvv <pphhww5017@gmail.com>
2023-06-14 16:23:56 +0900

    fix: OrderService의 order 메서드 접근 제한자 수정

--- jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
+++ jpashop/src/main/java/jpabook/jpashop/service/OrderService.java
@@ -27,7 +27,7 @@ public class OrderService {
      * 주문
      */
     @Transactional
-    private Long order(Long memberId, Long itemId, int count) {
+    public Long order(Long memberId, Long itemId, int count) {
 
         // 엔티티 조회
         Member member = memberRepository.findOne(memberId);
