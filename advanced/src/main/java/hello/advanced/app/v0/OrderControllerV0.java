d516e9512314e1b0d7f7224af6a88f7a99d6e408
Hyeonvv <pphhww5017@gmail.com>
2024-06-06 16:59:48 +0900

    add: 예제 프로젝트 생성

---
+++ advanced/src/main/java/hello/advanced/app/v0/OrderControllerV0.java
@@ -0,0 +1,21 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderControllerV0 {
+
+    private final OrderServiceV0 orderService;
+
+    @GetMapping("/v0/request")
+    public String request(@RequestParam(name = "itemId") String itemId) {
+
+        orderService.orderItem(itemId);
+
+        return "ok";
+    }
+}
---
+++ advanced/src/main/java/hello/advanced/app/v0/OrderRepositoryV0.java
@@ -0,0 +1,28 @@
+package hello.advanced.app.v0;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV0 {
+
+    public void save(String itemId) {
+
+        // 저장 로직
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생");
+        }
+
+        sleep(1000);
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
