448072cf864292ea22ecf73365e5b3d26f3ecaf2
Hyeonvv <pphhww5017@gmail.com>
2024-06-14 15:45:02 +0900

    add: 필드 동기화 - 적용

---
+++ advanced/src/main/java/hello/advanced/app/v3/OrderRepositoryV3.java
@@ -0,0 +1,44 @@
+package hello.advanced.app.v3;
+
+import hello.advanced.trace.TraceStatus;
+import hello.advanced.trace.logtrace.LogTrace;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderRepositoryV3 {
+
+    private final LogTrace trace;
+
+    public void save(String itemId) {
+
+        TraceStatus status = null;
+        try {
+            status = trace.begin("OrderRepository.save()");
+
+            // 저장 로직
+            if (itemId.equals("ex")) {
+                throw new IllegalStateException("예외 발생");
+            }
+
+            sleep(1000);
+
+            trace.end(status);
+        } catch (Exception e) {
+            trace.exception(status, e);
+
+            throw e; // 하위단에서 터진 예외를 꼭 다시 던져주어야 한다.
+        }
+
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
