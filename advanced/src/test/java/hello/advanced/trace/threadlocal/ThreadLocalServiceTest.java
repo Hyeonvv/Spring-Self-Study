cd1cf8037365114bac22a38f749156389b797855
Hyeonvv <pphhww5017@gmail.com>
2024-06-14 17:03:45 +0900

    add: ThreadLocal - 예제 코드

---
+++ advanced/src/test/java/hello/advanced/trace/threadlocal/ThreadLocalServiceTest.java
@@ -0,0 +1,48 @@
+package hello.advanced.trace.threadlocal;
+
+import hello.advanced.trace.threadlocal.code.FieldService;
+import hello.advanced.trace.threadlocal.code.ThreadLocalService;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ThreadLocalServiceTest {
+
+    private ThreadLocalService service = new ThreadLocalService();
+
+    @Test
+    void field() {
+        log.info("main start");
+
+        Runnable userA = () -> {
+            service.logic("userA");
+        };
+
+        Runnable userB = () -> {
+            service.logic("userB");
+        };
+
+        Thread threadA = new Thread(userA);
+        threadA.setName("thread-A");
+
+        Thread threadB = new Thread(userB);
+        threadB.setName("thread-B");
+
+        threadA.start();
+//        sleep(2000); // 동시성 문제 발생 X
+        sleep(100); // 동시성 문제 발생 O
+        threadB.start();
+
+        sleep(3000);
+        log.info("main exit");
+    }
+
+    private void sleep(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+}
